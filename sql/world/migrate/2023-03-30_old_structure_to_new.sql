b6cc79e2ec4a9d87d52169519fb30263bfaf6db8
hasn <h.allgaeuer@hotmail.de>
2023-09-03 00:21:40 +0200

    Move SQL to correct location

--- sql/world/migrate/2023-03-30_old_structure_to_new.sql
+++
@@ -1,5 +0,0 @@
-INSERT IGNORE INTO mod_quickbalance_modifier_creature (CreatureEntry, Difficulty, DamageModifier, HealthModifier, ManaModifier, ArmorModifier, Comment)
-SELECT  Creature, 0, DamageModifier, HealthModifier, ManaModifier, ArmorModifier, Comment FROM mod_quickbalance_modifier WHERE Creature > 0;
-
-INSERT IGNORE INTO mod_quickbalance_modifier_map (Map, Difficulty, DamageModifier, HealthModifier, ManaModifier, ArmorModifier, Comment)
-SELECT  Map, 0, DamageModifier, HealthModifier, ManaModifier, ArmorModifier, Comment FROM mod_quickbalance_modifier WHERE Map > 0 AND Creature = 0;
